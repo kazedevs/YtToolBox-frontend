22657f5a453a103c4f2ed15eff70ae19b3cff13a
kazedevs <businessrajkumardas@gmail.com>
2025-08-03 21:21:33 +0530

    feat: complete blog system with MDX content and components
    - Add complete MDX blog content files
    - Add BlogImage component for optimized image handling
    - Update App.tsx with blog routing
    - Configure build settings for MDX support
    - Complete blog SEO and UI optimization suite

--- yttoolbox/src/App.tsx
+++ yttoolbox/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 import ScrollToTop from './components/ScrollToTop';
 import Layout from "./components/Layout";
 import LandingPage from "./pages/LandingPage";
@@ -10,29 +11,41 @@ import ErrorPage from "./pages/ErrorPage";
 import { ThumbnailDownloader, PFPDownloader, BannerDownloader, CommentsExtractor } from "./pages/tools";
 import TitleExtractor from "./pages/tools/TitleExtractor";
 import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 
 const App = () => {
   return (
-    <Layout>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<TermsOfService />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-        {/* Tools Routes */}
-        <Route path="/tools/thumbnail-downloader" element={<ThumbnailDownloader />} />
-        
-        <Route path="/tools/pfp-downloader" element={<PFPDownloader />} />
-        <Route path="/tools/banner-downloader" element={<BannerDownloader />} />
-        <Route path="/tools/comments-extractor" element={<CommentsExtractor />} />
-        <Route path="/tools/title-extractor" element={<TitleExtractor />} />
-        <Route path="/404" element={<ErrorPage />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Routes>
-    </Layout>
+    <HelmetProvider>
+      <Layout>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          {/* Tools Routes */}
+          <Route path="/tools/thumbnail-downloader" element={<ThumbnailDownloader />} />
+          
+          <Route path="/tools/pfp-downloader" element={<PFPDownloader />} />
+          <Route path="/tools/banner-downloader" element={<BannerDownloader />} />
+          <Route path="/tools/comments-extractor" element={<CommentsExtractor />} />
+          <Route path="/tools/title-extractor" element={<TitleExtractor />} />
+          
+          {/* Redirect old routes */}
+          <Route path="/thumbnail-downloader" element={<Navigate to="/tools/thumbnail-downloader" replace />} />
+          <Route path="/pfp-downloader" element={<Navigate to="/tools/pfp-downloader" replace />} />
+          <Route path="/banner-downloader" element={<Navigate to="/tools/banner-downloader" replace />} />
+          <Route path="/comments-extractor" element={<Navigate to="/tools/comments-extractor" replace />} />
+          <Route path="/title-extractor" element={<Navigate to="/tools/title-extractor" replace />} />
+          
+          {/* Catch all route */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Layout>
+    </HelmetProvider>
   );
 };
 
--- yttoolbox/vite.config.ts
+++ yttoolbox/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdx()],
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
