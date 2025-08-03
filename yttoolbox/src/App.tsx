0ceeb67c0f582998c8802bdb15209e764a9462af
kazedevs <businessrajkumardas@gmail.com>
2025-08-03 14:08:16 +0530

    Revert "feat: optimize bundle size and performance"
    This reverts commit 2dd32ac22465c846f8d9a05579d10c757ed6a39f.

--- yttoolbox/src/App.tsx
+++ yttoolbox/src/App.tsx
@@ -1,50 +1,37 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { lazy, Suspense } from 'react';
 import ScrollToTop from './components/ScrollToTop';
 import Layout from "./components/Layout";
-
-// Lazy load all pages and tools
-const LandingPage = lazy(() => import("./pages/LandingPage"));
-const About = lazy(() => import("./pages/About"));
-const Privacy = lazy(() => import("./pages/Privacy"));
-const Contact = lazy(() => import("./pages/Contact"));
-const TermsOfService = lazy(() => import("./pages/TermsOfService"));
-const ErrorPage = lazy(() => import("./pages/ErrorPage"));
-const Blog = lazy(() => import("./pages/Blog"));
-
-// Lazy load tools
-const ThumbnailDownloader = lazy(() => import("./pages/tools/ThumbnailDownloader"));
-const PFPDownloader = lazy(() => import("./pages/tools/PFPDownloader"));
-const BannerDownloader = lazy(() => import("./pages/tools/BannerDownloader"));
-const CommentsExtractor = lazy(() => import("./pages/tools/CommentsExtractor"));
-const TitleExtractor = lazy(() => import("./pages/tools/TitleExtractor"));
+import LandingPage from "./pages/LandingPage";
+import About from "./pages/About";
+import Privacy from "./pages/Privacy";
+import Contact from "./pages/Contact";
+import TermsOfService from "./pages/TermsOfService";
+import ErrorPage from "./pages/ErrorPage";
+import { ThumbnailDownloader, PFPDownloader, BannerDownloader, CommentsExtractor } from "./pages/tools";
+import TitleExtractor from "./pages/tools/TitleExtractor";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
     <Layout>
       <ScrollToTop />
-      <Suspense fallback={
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-        </div>
-      }>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<TermsOfService />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          {/* Tools Routes */}
-          <Route path="/tools/thumbnail-downloader" element={<ThumbnailDownloader />} />
-          <Route path="/tools/pfp-downloader" element={<PFPDownloader />} />
-          <Route path="/tools/banner-downloader" element={<BannerDownloader />} />
-          <Route path="/tools/comments-extractor" element={<CommentsExtractor />} />
-          <Route path="/tools/title-extractor" element={<TitleExtractor />} />
-          <Route path="/404" element={<ErrorPage />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* Tools Routes */}
+        <Route path="/tools/thumbnail-downloader" element={<ThumbnailDownloader />} />
+        
+        <Route path="/tools/pfp-downloader" element={<PFPDownloader />} />
+        <Route path="/tools/banner-downloader" element={<BannerDownloader />} />
+        <Route path="/tools/comments-extractor" element={<CommentsExtractor />} />
+        <Route path="/tools/title-extractor" element={<TitleExtractor />} />
+        <Route path="/404" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
     </Layout>
   );
 };
